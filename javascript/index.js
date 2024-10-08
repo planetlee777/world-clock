e1c3db4b56656443710d7646ade70bc7366e0f6e
planetlee777 <128845585+planetlee777@users.noreply.github.com>
2024-10-08 13:59:30 -0400

    add code

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,7 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = moment().format("dddd");
+let losAngelesTime = moment();
+
+losAngelesDateElement.innerHTML = losAngelesTime().format("MMM Do YYYY");
 losAngelesTimeElement.innerHTML = "1:49:15<small>AM</small>";
