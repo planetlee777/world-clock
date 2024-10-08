d43f2bd84319a37f09ad380ee6dcbcee4846cd61
planetlee777 <128845585+planetlee777@users.noreply.github.com>
2024-10-08 15:04:59 -0400

    add code

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,21 @@
+//los angeles
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment();
+let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMM Do YYYY");
 losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+	"h:mm:ss [<small>]A[</small>]"
+)}`;
+
+//paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+
+parisElement.innerHTML = parisTime.format("MMM Do YYYY");
+parisTimeElement.innerHTML = `${parisTime.format(
 	"h:mm:ss [<small>]A[</small>]"
 )}`;
